d4e2c708914e185584047997fac3daa753d02d3a
talha <talhagedikli@protonmail.com>
2021-05-20 00:08:27 +0300

    Update

--- Projects/Equinox/Scripts/Player.gd
+++ Projects/Equinox/Scripts/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var up : Vector2 = Vector2(0, -1)
 var gravity : float = 6.00
+var direction: Vector2 = Vector2.ZERO
 
 var maxFallSpeed = 175
 var maxSpeed = 150
@@ -16,7 +17,7 @@ var jumpBufferMax = 6
 var onGround : bool = false
 var on_wall = false
 
-var gas_max = 150
+export var gas_max = 200
 var gas : float = gas_max
 var gas_rate : float = gas / gas_max
 var pack_power : float = 0.00
@@ -27,6 +28,7 @@ var accel = 5
 var decel = 8
 
 var facing = 1
+var last_facing = facing
 var motion : Vector2 = Vector2()
 
 enum playerStates {
@@ -41,6 +43,7 @@ var state = playerStates.MOVE
 var key_hold = "z"
 var key_pack = "alt"
 var key_jump = "space"
+var key_dash = "x"
 
 onready var sprite = get_node("Pivot/Sprite")
 onready var pivot = get_node("Pivot")
@@ -49,6 +52,7 @@ onready var raycast_bottom : RayCast2D = $Collisions/BottomCollision
 onready var raycast_top : RayCast2D = $Collisions/TopCollision
 onready var raycast_left : RayCast2D = $Collisions/LeftCollision
 onready var raycast_right : RayCast2D = $Collisions/RightCollision
+onready var dashTween : Tween = $Tween
 
 signal using_pack
 
@@ -59,7 +63,7 @@ func _ready():
 func _physics_process(delta):
 	
 	onGround = raycast_bottom.is_colliding()
-	if raycast_right.is_colliding() or raycast_left.is_colliding(): 
+	if raycast_right.is_colliding() or raycast_left.is_colliding():
 		on_wall = true
 	else:
 		on_wall = false
@@ -69,9 +73,10 @@ func _physics_process(delta):
 #		motion.y = maxFallSpeed
 	if state == playerStates.MOVE:
 		player_state_move()
-		
 	elif state == playerStates.HOLD:
 		player_state_hold()
+	elif state == playerStates.DASH:
+		player_state_dash(delta)
 	
 	motion = move_and_slide(motion, up)
 	
@@ -79,6 +84,8 @@ func _physics_process(delta):
 	if motion.x > 0: facing = 1
 	elif motion.x < 0: facing = -1
 	else: facing = facing
+	
+	direction = motion.normalized()
 	
 	#Animation
 	body.scale.x = lerp(sprite.scale.x, 1, 0.5)
@@ -93,6 +100,8 @@ func squashAndStretch(xscale : float, yscale : float):
 
 #States
 func player_state_move():
+	set_direction()
+	
 	if Input.is_action_pressed("right"):
 		motion.x += accel
 	elif Input.is_action_pressed("left"):
@@ -145,21 +154,61 @@ func player_state_move():
 	
 	if !onGround : motion.y += gravity
 	
+	last_facing = facing
+	
 	motion.x = clamp(motion.x, -maxSpeed, maxSpeed)
 	motion.y = clamp(motion.y, -maxFallSpeed, maxFallSpeed)
 	
 	#Switch statement
 	if Input.is_action_just_pressed(key_hold) && on_wall: state = playerStates.HOLD
-
+	if Input.is_action_just_pressed(key_dash): 
+		state = playerStates.DASH
+		$Timer.start()
 func player_state_hold():
+	if raycast_right.is_colliding(): facing = -1
+	else: facing = 1
 	motion.x = 0
 	motion.y = lerp(motion.y, 0, 0.2)
 	if Input.is_action_just_pressed(key_jump): 
-		motion.x = -jumpForce * facing * 0.7
+		motion.x = jumpForce * facing * 0.7
 		motion.y = -jumpForce * 0.8
-		state = playerStates.MOVE
-	if Input.is_action_just_released(key_hold): state = playerStates.MOVE
+		facing = last_facing
+		state = playerStates.MOVE	
+	if Input.is_action_just_released(key_hold): 
+		facing = last_facing
+		state = playerStates.MOVE		
+
+func player_state_dash(delta):
+	var move_to = direction * Vector2(jumpForce*5, jumpForce*5)
+#	var dif = motion.distance_to(move_to)
+##	dashTween.interpolate_property(self, "position", self.position, move_to, 0.2, Tween.TRANS_QUINT, Tween.EASE_IN)
+##	if !dashTween.is_active(): dashTween.start()
+#	if dif > 1: motion.move_toward(move_to, delta)
+#	else: state = playerStates.MOVE
+
+func set_direction():
+	if Input.is_action_pressed("right"):
+		direction.x = 1
+	elif Input.is_action_pressed("left"):
+		direction.x = -1
+	else:
+		direction.x = 0
+	
+	if Input.is_action_pressed("up"):
+		direction.y = 1
+	elif Input.is_action_pressed("down"):
+		direction.y = -1
+	else:
+		direction.y = 0
+func get_direction():
+	return direction
 	
 
-	
-	
+
+
+#func _on_Tween_tween_completed(self, "Player/dashTween"):
+#	state = playerStates.MOVE
+
+
+func _on_Timer_timeout():
+	state = playerStates.MOVE
--- Projects/Equinox/Scripts/PlayerUI.gd
+++ Projects/Equinox/Scripts/PlayerUI.gd
@@ -1,20 +1,33 @@
 extends CanvasLayer
 
-onready var TweenNode = get_node("GasRate/Tween")
 
-onready var gasRate : Node = $GasRate
 var gas_rate : float = 0.00 setget set_gas_rate, get_gas_rate
 var gas_rate_a = 1 setget set_gas_rate_a, get_gas_rate_a
+var counter: float = 0
+
+onready var TweenNode = get_node("GasRate/Tween")
+onready var gasRate : Node = $GasRate
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	#Set gas rate hud
-#	if gas_rate < 0.4: 
-#		gas_rate_a = TweenNode.interpolate_method(self, "set_gas_rate_a", 0, 1, 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT, 0)
+#	if gas_rate < 0.6: 
+#		TweenNode.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
 #		TweenNode.start()
 #	else:
 #		TweenNode.stop(self)
-#		gas_rate_a = 1
+#		gasRate.modulate.a = 1
+	if gas_rate < 0.4:
+		counter += 1
+		gasRate.modulate.a = sin(counter/2)
+	else:
+		gasRate.modulate.a = 1
+		counter = 0
+	
 	gasRate.rect_scale.x = lerp(gasRate.rect_scale.x, gas_rate, 0.1) + 0.0001
+	
 #	gasRate.rect_clip_content.color.set_frame_color(1, 1, 1, gas_rate_a)
 
 #Functions
